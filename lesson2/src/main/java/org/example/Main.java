a46db89f7c70167019307d77e8e1ba17b1a2f15a
Egor.Efimenko <20_Morokey_04@mail.ru>
2023-10-08 19:03:31 +0300

    test changes from TestBranch1

--- lesson2/src/main/java/org/example/Main.java
+++ lesson2/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("testing changes");
+        System.out.println("testing from testBranch1");
     }
 }
